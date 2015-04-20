eeb7af0b64bdaff4235117aa85d1f8cdcc6d208d
bredtoshred <phillip.mckenzie@colorado.edu>
2015-04-20 12:16:14 -0600

    Update SocMeet.cpp

--- SocMeet.cpp
+++ SocMeet.cpp
@@ -1,1 +1,37 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include "friendFuncs.h"
+using namespace std;
 
+int main(int argc, char*argv[])
+{
+	void displayMenu();
+	cin.ignore();
+	getline(cin, cmd);
+	
+	while(cmd == "create" || cmd == "add" || cmd == "delete" || cmd == "DOB" || cmd == "depth")
+	{
+		if( cmd == "create")
+		{
+			
+		}
+		else if( cmd == "add")
+		{
+			
+		}
+		else if( cmd == "delete")
+		{
+			
+		}
+		else if( cmd == "DOB")
+		{
+			
+		}
+		else if( cmd == "depth")
+		{
+			
+		}
+}
+
+cout<< "Please enter valid command" << endl;
