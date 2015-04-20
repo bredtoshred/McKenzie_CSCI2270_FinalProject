02ee8ecc868c6f41122f901f8243ea5411f81f5d
bredtoshred <phillip.mckenzie@colorado.edu>
2015-04-20 14:57:42 -0600

    Update friendFuncs.cpp

--- friendFuncs.cpp
+++ friendFuncs.cpp
@@ -1,8 +1,16 @@
+//Phil, Julio, Marcus
 #include <iostream>
 #include <fstream>
 #include <string>
 #include "friendFuncs.h"
 using namespace std;
+
+//meet the variables
+string newName;
+int newDOB;
+
+
+
 
 void friendGraph::displayMenu()
 {
@@ -11,7 +19,28 @@ void friendGraph::displayMenu()
 	cout<< " Create an Account? (create) " << endl;
 	cout<< " Add a friend? (add) " << endl;
 	cout<< " Delete a friend? (delete) "<< endl;
-	cout<< " Check a friends birthday? (DOB) " << endl
+	cout<< " Check a friends birthday? (dob) " << endl;
 	cout<< " Check how many friends seperate you and another account? (depth) " << endl;
+}
+
+void friendGraph::createAccount();
+{
+	cout<< "create" <<endl;
+}
+void friendGraph::addFriend();
+{
+	cout<<"add"<<endl;
+}
+void friendGraph::deleteFriend();
+{
+	cout<<"delete"<<endl;
+}
+void friendGraph::checkDOB();
+{
+	cout << "dob" << endl;
+}
+void friendGraph::checkDepth();
+{
+	cout << "depth" endl;
 }
 	
