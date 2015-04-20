675f50c45884e9472c728ba8e373a7fc1692b91d
bredtoshred <phillip.mckenzie@colorado.edu>
2015-04-20 12:16:56 -0600

    Update friendFuncs.cpp

--- friendFuncs.cpp
+++ friendFuncs.cpp
@@ -1,1 +1,17 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include "friendFuncs.h"
+using namespace std;
 
+void friendGraph::displayMenu()
+{
+	cout<< " Welcome to Social Media" << endl;
+	cout<< " Would you like to... " <<endl;
+	cout<< " Create an Account? (create) " << endl;
+	cout<< " Add a friend? (add) " << endl;
+	cout<< " Delete a friend? (delete) "<< endl;
+	cout<< " Check a friends birthday? (DOB) " << endl
+	cout<< " Check how many friends seperate you and another account? (depth) " << endl;
+}
+	
