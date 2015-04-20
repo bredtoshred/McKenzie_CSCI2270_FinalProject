4683daf261bf8fc5403239062b35a01628817a16
bredtoshred <phillip.mckenzie@colorado.edu>
2015-04-20 12:17:46 -0600

    Update friendFuncs.h

--- friendFuncs/friendFuncs.h
+++ friendFuncs/friendFuncs.h
@@ -1,1 +1,23 @@
+#ifndef FRIENDFUNCS_H
+#define FRIENDFUNCS_H
+#include <string>
 
+class friendGraph
+{
+	public:
+	friendGraph();
+	~friendGraph();
+	void displayMenu();
+	
+};
+
+
+/*struct friend
+{
+	std::string name;
+	int DOB;
+};
+*/
+
+
+#endif 
